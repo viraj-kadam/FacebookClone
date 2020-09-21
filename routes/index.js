5779fd175666e522933d9c985c85eca39cd349d9
Viraj kadam <99virajsk@gmail.com>
2020-09-21 22:07:22 +0530

    Viraj | Added login page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Facebook Clone' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Facebook Clone' });
+});
+
 module.exports = router;
